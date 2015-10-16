cca3ff13b44809eee481d741f580ec12498f8011
AsciiSmoke <AsciiSmoke@Gmail.com>
2015-10-16 15:41:55 +0100

    Update loadTemplate.js

--- loadTemplate.js
+++ loadTemplate.js
@@ -1,60 +1,78 @@
-var LoadTemplate = function(element, template, data){
-    // Check if parenet element is defined as string or object.
-    if(typeof element == 'string'){
-        this.el = document.getElementById(element);
-    } else {
-        this.el = element;
-    }
+(function (SubTemplater, $) {
 
-    // Store template name and data.
-    this.tempName = template;
-    this.data = data || null;
+    var LoadTemplate = function (selector, template, data) {
 
-    // You can change this to path of your template folder.
-    this.folderPath = 'assets/templates/';
-};
+        this.selector = selector;
 
-LoadTemplate.prototype.create = function(callback){
-    var req = new XMLHttpRequest();
-    var that = this;
+        // Store template name and data.
+        this.tempName = template;
+        this.data = data || null;
 
-    // Define parameters for request.
-    req.open('get', this.folderPath + this.tempName + '.handlebars', true);
-
-    // Wait for request to complete.
-    req.onreadystatechange = function(){
-        if (req.readyState == 4 && req.status == 200){
-            //Compile HB template, add data (if defined) and place in parent element.
-            var compiled = Handlebars.compile(req.response);
-            that.el.innerHTML = compiled(that.data);
-
-            // Execute callback function
-            callback();
-        }
     };
 
-    // Send request.
-    req.send();
-};
+    LoadTemplate.prototype.create = function (callback) {
+        var req = new XMLHttpRequest();
+        var that = this;
 
-LoadTemplate.prototype.createAndWait = function(callback){
-    var req = new XMLHttpRequest();
-    var that = this;
-
-    // Define parameters for request.
-    req.open('get', this.folderPath + this.tempName + '.handlebars', true);
-
-    // Wait for request to complete.
-    req.onreadystatechange = function(){
-        if (req.readyState == 4 && req.status == 200){
-            //Compile HB template, but wait..
-            var compiled = Handlebars.compile(req.response);
-
-            // Execute callback function and parse variables.
-            callback(compiled, that.el);
+        // check if the file name already has an extension.
+        if (
+            this.tempName.indexOf('.handlebars', this.length - '.handlebars'.length) === -1
+            && this.tempName.indexOf('.mustache', this.length - '.mustache'.length) === -1
+        ) {
+            this.tempName += '.handlebars';
         }
+
+        req.open('get', this.tempName, true);
+
+        // Wait for request to complete.
+        req.onreadystatechange = function () {
+            if (req.readyState == 4 && req.status == 200) {
+                //Compile HB template, add data (if defined) and place in parent element.
+                var compiled = Handlebars.compile(req.response);
+                $(that.selector).replaceWith(compiled(that.data));
+
+                // Execute callback function
+                if (typeof callback === "function") {
+                    callback();
+                }
+            }
+        };
+
+        // Send request.
+        req.send();
     };
 
-    // Send request.
-    req.send();
-};
+    LoadTemplate.prototype.createAndWait = function (callback) {
+        var req = new XMLHttpRequest();
+        var that = this;
+
+        // Define parameters for request.
+        req.open('get', this.tempName + '.handlebars', true);
+
+        // Wait for request to complete.
+        req.onreadystatechange = function () {
+            if (req.readyState == 4 && req.status == 200) {
+                //Compile HB template, but wait..
+                var compiled = Handlebars.compile(req.response);
+
+                // Execute callback function and parse variables.
+                callback(compiled, that.selector);
+            }
+        };
+
+        // Send request.
+        req.send();
+    };
+
+    // Wait until document.ready is finished
+    $(window).load(function () {
+        $("template[type='handlebars']").each(function (i, e) {
+            var $e = $(e);
+            var name = 'Template[name="' + $e.attr("name") + '"]';
+            var src = $e.data("src");
+            var options = $e.data("options").replace("'", '"');
+            new LoadTemplate(name, src, options).create();
+        });
+    });
+
+}(window.SubTemplater = window.SubTemplater || {}, jQuery));
